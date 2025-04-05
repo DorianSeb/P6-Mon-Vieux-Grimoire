3380e3f3ade4e32f47f9e832e440ef953c92150f
DorianSeb <dorian.sebille@hotmail.fr>
2025-04-05 19:22:16 +0200

    Corrections finales : ajout de dotenv, multer et fix auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 module.exports = (req, res, next) => {
     try {
         // Récupère le token dans le header Authorization (format : "Bearer TOKEN")
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
         req.auth = {
             userId: decodedToken.userId
